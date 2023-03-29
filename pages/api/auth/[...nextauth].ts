991f31b4e2f8b145f4e620828abda9ea4bfd02cb
Eri Schon <erischon@gmail.com>
2023-03-29 18:08:24 +0200

    auth with github & google

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,23 @@
+import { compare } from "bcrypt";
+
 import NextAuth from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
-import { compare } from "bcrypt";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { prisma } from "../../../lib/db";
 
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
@@ -45,6 +57,7 @@ export default NextAuth({
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,6 +5,9 @@ import { signIn } from "next-auth/react";
 
 import Input from "../components/Input";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 export default function Auth() {
   const router = useRouter();
 
@@ -48,16 +51,17 @@ export default function Auth() {
   }, [email, name, password, login]);
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="logo" className="h-12" />
+    <div className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-white w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-4 flex items-end gap-1 bg-slate-300">
+          <img src="/images/logo.png" alt="logo" className="h-8" />
+          <span className="text-xl font-semibold">OC</span>
         </nav>
 
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+          <div className="bg-[var(--color-primary)] bg-opacity-30 px-16 py-10 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign in" : "Register"}
+              {variant === "login" ? "Se connecter" : "S'enregistrer"}
             </h2>
 
             <div className="flex flex-col gap-4">
@@ -98,18 +102,34 @@ export default function Auth() {
               onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
-              {variant === "login" ? "Sign in" : "Sign up"}
+              {variant === "login" ? "Vous connecter" : "Vous enregistrer"}
             </button>
 
-            <p className="text-neutral-500 mt-12">
+            <div className="flex flex-row items-center justify-center gap-4 mt-8">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
+              </div>
+
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
+
+            <p className="text-neutral-300 mt-12">
               {variant === "login"
-                ? "New to Netflix?"
-                : "Already have an account?"}
+                ? "Nouveau sur BOC ?"
+                : "Vous avez déjà un compte ?"}
               <span
                 onClick={toggleVariant}
                 className="text-white ml-1 hover:underline cursor-pointer"
               >
-                {variant === "login" ? "Sign up now." : "Sign in now."}
+                {variant === "login" ? "Vous enregistrer." : "Vous connecter."}
               </span>
             </p>
           </div>
