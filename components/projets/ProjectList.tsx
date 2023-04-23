b7e445db5452a87795e5cf1181f619179c723100
Eri Schon <erischon@gmail.com>
2023-04-23 19:45:56 +0200

    Update ProjectList.tsx

--- components/projets/ProjectList.tsx
+++ components/projets/ProjectList.tsx
@@ -3,14 +3,15 @@
 import { useQuery } from "@apollo/client";
 
 import { GET_ACTIVE_PROJECTS } from "@/graphql/queries";
+import Link from "next/link";
 
 interface ProjectListProps {
   userId: string;
 }
 
 const PROJECT_TYPE: any = {
-  INTERNAL: "bg-yellow-50",
-  EXTERNAL: "bg-white",
+  INTERNAL: { acronym: "IN", color: "bg-yellow-50" },
+  EXTERNAL: { acronym: "EX", color: "bg-white" },
 };
 
 /**
@@ -35,12 +36,21 @@ const ProjectList: React.FC<ProjectListProps> = ({ userId }) => {
   return (
     <section className="w-full">
       {data.getActiveUserProjects.map((project: any, key: any) => (
-        <div key={key} className="grid grid-cols-2 w-full">
-          <div className={`${projectType(project.projectType)}`}>
-            {project.name}
+        <div key={key} className="flex gap-2 w-full">
+          <div
+            className={`${
+              projectType(project.projectType).color
+            } w-6 flex items-center justify-center text-xs font-semibold`}
+          >
+            {projectType(project.projectType).acronym}
           </div>
 
-          <div>{project.client.name}</div>
+          <div>
+            <Link href="">
+              <h3 className="font-semibold">{project.name}</h3>
+              <p className="text-sm font-extralight">{project.client.name}</p>
+            </Link>
+          </div>
         </div>
       ))}
     </section>
